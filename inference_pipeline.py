cba382daac91aa313a09fd319ad61d3e5c172685
Kabeer Akande <46060542+kbakande@users.noreply.github.com>
2024-02-07 08:48:41 +0000

    catching enabled

--- inference_pipeline.py
+++ inference_pipeline.py
@@ -42,5 +42,5 @@ if __name__ == "__main__":
             "table_ref": table_ref, 
             "project": project_id  
         },
-        enable_caching=True
+        enable_caching=False
     ).submit(service_account=service_account)
